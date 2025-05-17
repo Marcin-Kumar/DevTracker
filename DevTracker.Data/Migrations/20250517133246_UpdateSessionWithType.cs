98cb15fe2387b8df1f98cddcd39b035d1d7af207
Marcin-Kumar <marcinkumar@gmail.com>
2025-05-17 15:35:30 +0200

    Added migration for session type

---
+++ DevTracker.Data/Migrations/20250517133246_UpdateSessionWithType.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSessionWithType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Sessions",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Sessions");
+        }
+    }
+}
